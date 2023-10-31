1b2f4468899a1c867388d416da6d3b323f95c592
Russell Soza <endosoza1@gmail.com>
2023-10-31 16:45:44 -0600

    feat: Add partograph list to home screen

--- lib/src/presentation/home/home_screen.dart
+++ lib/src/presentation/home/home_screen.dart
@@ -15,7 +15,9 @@ class _HomeState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BirthFlow'),
+        elevation: 2,
         actions: <Widget>[
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
           PopupMenuButton<_Options>(
             padding: const EdgeInsets.all(12),
             position: PopupMenuPosition.under,
@@ -34,6 +36,77 @@ class _HomeState extends State<HomeScreen> {
                 child: Text('Informacion'),
               ),
             ],
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == 0) {
+                  return const ListTile(
+                    leading: Icon(Icons.archive),
+                    title: Text('Expediente'),
+                  );
+                } else {
+                  return ListTile(
+                    titleAlignment: ListTileTitleAlignment.center,
+                    title: Text('Partograma $index'),
+                    trailing:  const SizedBox(
+                      width:
+                          100, 
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('01/01/2023'),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              if (true) Icon(Icons.push_pin),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: const Text('012HMK231D2095 - 01/01/2023'),
+                    onLongPress: () => showModalBottomSheet<void>(
+                      context: context,
+                      showDragHandle: true,
+                      builder: (BuildContext context) {
+                        return SizedBox(
+                          height: 120,
+                          child: Center(
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      icon: const Icon(Icons.push_pin),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.delete),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+              childCount: 100,
+            ),
           ),
         ],
       ),
