4e48221b6281684f232385338107f133afc2e005
Russell Soza <endosoza1@gmail.com>
2023-10-30 22:49:19 -0600

    refactor: Add app dev class and update main.dart

---
+++ lib/src/app_dev.dart
@@ -0,0 +1,17 @@
+import 'package:birthflow/src/config/router/app_router.dart';
+import 'package:flutter/material.dart';
+
+class AppDev extends StatelessWidget {            
+          
+  final _appRouter = AppRouter();            
+            
+  @override            
+  Widget build(BuildContext context){            
+    return MaterialApp.router(       
+      title: 'Birth Flow',
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,     
+      routerConfig: _appRouter.config(),         
+    );            
+  }            
+}            
