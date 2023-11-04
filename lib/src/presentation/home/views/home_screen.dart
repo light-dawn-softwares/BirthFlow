77e245184a65db4f06a944c2006ac1ff9e0220d4
Russell Eduardo Soza Arevalo <endosoza1@gmail.com>
2023-11-03 21:32:04 -0600

    feat: Update home and new partograph screens (#29)

--- lib/src/presentation/home/views/home_screen.dart
+++ lib/src/presentation/home/views/home_screen.dart
@@ -1,4 +1,5 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:birthflow/src/config/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -9,7 +10,15 @@ class HomeScreen extends StatefulWidget {
 
 enum _Options { newPartograph, configuration, information }
 
-class _HomeState extends State<HomeScreen> {
+class _HomeState extends State<HomeScreen> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 1, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +30,15 @@ class _HomeState extends State<HomeScreen> {
           PopupMenuButton<_Options>(
             padding: const EdgeInsets.all(12),
             position: PopupMenuPosition.under,
-            onSelected: (_Options item) {},
+            onSelected: (_Options item) {
+              if (item == _Options.newPartograph) {
+                context.router.push(const NewPartographRoute());
+              }
+
+              if (item == _Options.configuration) {
+                context.router.push(const ConfigurationRoute());
+              }
+            },
             itemBuilder: (BuildContext context) => <PopupMenuEntry<_Options>>[
               const PopupMenuItem<_Options>(
                 value: _Options.newPartograph,
@@ -38,80 +55,105 @@ class _HomeState extends State<HomeScreen> {
             ],
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(
+            kToolbarHeight,
+          ), // Ajusta la altura según tu diseño
+          child: SizedBox(
+            width: double.infinity,
+            child: TabBar(
+              controller: _tabController,
+              isScrollable: true,
+              tabs: const <Widget>[
+                Tab(
+                  text: 'Partogramas',
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                if (index == 0) {
-                  return const ListTile(
-                    leading: Icon(Icons.archive),
-                    title: Text('Expediente'),
-                  );
-                } else {
-                  return ListTile(
-                    titleAlignment: ListTileTitleAlignment.center,
-                    title: Text('Partograma $index'),
-                    trailing: const SizedBox(
-                      width: 100,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('01/01/2023'),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (index == 0) {
+                      return const ListTile(
+                        leading: Icon(Icons.archive),
+                        title: Text('Archivados'),
+                      );
+                    } else {
+                      return ListTile(
+                        titleAlignment: ListTileTitleAlignment.center,
+                        title: Text('Partograma $index'),
+                        trailing: const SizedBox(
+                          width: 100,
+                          child: Column(
                             crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              if (true) Icon(Icons.push_pin),
+                              Text('01/01/2023'),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  if (true) Icon(Icons.push_pin),
+                                ],
+                              ),
                             ],
                           ),
-                        ],
-                      ),
-                    ),
-                    subtitle: const Text('012HMK231D2095 - 01/01/2023'),
-                    onLongPress: () => showModalBottomSheet<void>(
-                      context: context,
-                      showDragHandle: true,
-                      builder: (BuildContext context) {
-                        return SizedBox(
-                          height: 120,
-                          child: Center(
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      icon: const Icon(Icons.push_pin),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.delete),
+                        ),
+                        subtitle: const Text('012HMK231D2095 - 01/01/2023'),
+                        onLongPress: () => showModalBottomSheet<void>(
+                          context: context,
+                          showDragHandle: true,
+                          builder: (BuildContext context) {
+                            return SizedBox(
+                              height: 120,
+                              child: Center(
+                                child: Column(
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          icon: const Icon(Icons.push_pin),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(Icons.delete),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                }
-              },
-              childCount: 100,
-            ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
+                  childCount: 2,
+                ),
+              ),
+            ],
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Nuevo',
-        onPressed: () {},
+        onPressed: () {
+          context.router.push(const NewPartographRoute());
+        },
         child: const Icon(Icons.add),
       ),
     );
--- lib/src/presentation/new_partograph/views/new_partograph_screen.dart
+++ lib/src/presentation/new_partograph/views/new_partograph_screen.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:birthflow/src/core/work_time/domain/models/lib/membranas.dart';
+import 'package:birthflow/src/core/work_time/domain/models/lib/paridad.dart';
+import 'package:birthflow/src/core/work_time/domain/models/lib/posicion.dart';
+import 'package:birthflow/src/core/work_time/domain/models/work_time.dart';
+import 'package:birthflow/src/presentation/widgets/work_time/work_time_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 @RoutePage()
 class NewPartographScreen extends StatefulWidget {
@@ -7,13 +13,149 @@ class NewPartographScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _NewPartographState();
 }
 
-class _NewPartographState extends State<NewPartographScreen> {
+class _NewPartographState extends State<NewPartographScreen>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  final WorkTime workTime = WorkTime(
+    idPartograph: -1,
+    posicion: const Posicion.init(),
+    paridad: const Paridad.init(),
+    membranas: const Membranas.init(),
+  );
+
+  late TextEditingController _nameTextController;
+  late TextEditingController _recordNumberTextController;
+  late TextEditingController _dateTextController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _nameTextController = TextEditingController(text: '');
+    _recordNumberTextController = TextEditingController(text: '');
+    _dateTextController = TextEditingController(text: '');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nuevo Partograma'),
+    return WillPopScope(
+      onWillPop: () async {
+        final willPopScope = await showWarning(context);
+        return willPopScope ?? false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Nuevo Partograma'),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const <Widget>[
+              Tab(
+                text: 'Datos enerales',
+              ),
+              Tab(
+                text: 'Construccion de la curva de alerta',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextField(
+                      controller: _nameTextController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Nombre',
+                        hintText: 'Ingrese el nombre',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextField(
+                      controller: _recordNumberTextController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Expediente',
+                        hintText: 'Ingrese el numero de expediente',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      controller: _dateTextController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Tiempo',
+                      ),
+                      readOnly: true,
+                      onTap: () async {
+                        final DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(
+                            2000,
+                          ),
+                          lastDate: DateTime(2101),
+                        );
+
+                        if (pickedDate != null) {
+                          final String formattedDate =
+                              DateFormat('yyyy-MM-dd').format(pickedDate);
+
+                          setState(() {
+                            _dateTextController.text = formattedDate;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+              child: WorkTimeTableWidget(
+                currentWorkTime: workTime,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Future<bool?> showWarning(BuildContext context) async => showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Desea volver? '),
+          content:
+              const Text('Desea cancelar la creacion del nuevo partograma?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, false),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                FocusManager.instance.primaryFocus?.unfocus();
+                Navigator.pop(context, true);
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
 }
