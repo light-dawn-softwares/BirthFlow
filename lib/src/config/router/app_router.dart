b1bba1c0f23e1fe2ee07c792ba8b70408cc106bc
Russell Eduardo Soza Arevalo <endosoza1@gmail.com>
2023-11-08 14:59:25 -0600

    refactor: Add chart screen to router (#34)

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:birthflow/src/presentation/configuration/views/configuration_screen.dart';
 import 'package:birthflow/src/presentation/home/views/home_screen.dart';
 import 'package:birthflow/src/presentation/new_partograph/views/new_partograph_screen.dart';
+import 'package:birthflow/src/presentation/partograph/views/chart_screen.dart';
 import 'package:birthflow/src/presentation/partograph/views/partograph_screen.dart';
 
 part 'app_router.gr.dart';
@@ -16,6 +17,7 @@ class AppRouter extends _$AppRouter {
         ),
         AutoRoute(page: NewPartographRoute.page),
         AutoRoute(page: PartographRoute.page),
-        AutoRoute(page: ConfigurationRoute.page)
+        AutoRoute(page: ConfigurationRoute.page),
+        AutoRoute(page: ChartRoute.page),
       ];
 }
--- lib/src/config/router/app_router.gr.dart
+++ lib/src/config/router/app_router.gr.dart
@@ -15,6 +15,12 @@ abstract class _$AppRouter extends RootStackRouter {
 
   @override
   final Map<String, PageFactory> pagesMap = {
+    ChartRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: ChartScreen(),
+      );
+    },
     ConfigurationRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
@@ -40,6 +46,20 @@ abstract class _$AppRouter extends RootStackRouter {
       );
     },
   };
+}
+
+/// generated route for
+/// [ChartScreen]
+class ChartRoute extends PageRouteInfo<void> {
+  const ChartRoute({List<PageRouteInfo>? children})
+      : super(
+          ChartRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ChartRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
 /// generated route for
