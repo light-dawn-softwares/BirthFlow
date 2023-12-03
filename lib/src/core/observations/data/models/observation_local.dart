85431264ea15071090f40a811ea57e7a1af7a36f
Russell Soza <endosoza1@gmail.com>
2023-12-02 22:59:54 -0600

    refactor: Add foreign keys to dtos

--- lib/src/core/observations/data/models/observation_local.dart
+++ lib/src/core/observations/data/models/observation_local.dart
@@ -1,12 +1,27 @@
+import 'package:birthflow/src/core/general/data/models/general_local.dart';
 import 'package:floor/floor.dart';
 
-@entity
+@Entity(
+  tableName: 'vpps',
+  foreignKeys: [
+    ForeignKey(
+      childColumns: ['partograph_id'],
+      parentColumns: ['partograph_id'],
+      entity: GeneralDto,
+    ),
+  ],
+)
 class ObservationDto {
   @PrimaryKey(autoGenerate: true)
   final int? mSObservationID;
 
+  @ColumnInfo(name: 'partograph_id')
   final int partographID;
+  
+  @ColumnInfo(name: 'header')
   final String header;
+  
+  @ColumnInfo(name: 'description')
   final String description;
 
   ObservationDto({
