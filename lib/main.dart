e03e7d1b5f4b4360a2df600e758fb81251cc50cd
Russell Soza <endosoza1@gmail.com>
2023-12-04 21:38:42 -0600

    feat: Add blocProviders and initialize locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:birthflow/src/app_dev.dart';
+import 'package:birthflow/src/locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDependencies();
+
   runApp(
     AppDev(),
   );
