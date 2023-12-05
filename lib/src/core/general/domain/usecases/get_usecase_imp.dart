d51b59858ac1ed2d093dabddce52bfa1123e0a12
Russell Soza <endosoza1@gmail.com>
2023-12-04 18:46:17 -0600

    refactor: Modify general feat

--- lib/src/core/general/domain/usecases/get_usecase_imp.dart
+++ lib/src/core/general/domain/usecases/get_usecase_imp.dart
@@ -2,12 +2,12 @@ import 'package:birthflow/src/core/general/domain/models/general.dart';
 import 'package:birthflow/src/core/general/domain/repository/repository.dart';
 import 'package:birthflow/src/core/general/domain/usecases/get_usecase.dart';
 
-class GetGeneralUseCaseImplementation implements GetGeneralUseCase {
+class GeneralGetUseCaseImplementation implements GeneralGetUseCase {
   final GeneralRepository _repository;
-  const GetGeneralUseCaseImplementation(this._repository);
+  const GeneralGetUseCaseImplementation(this._repository);
 
   @override
-  Future<List<General>> execute() {
-    return _repository.get();
+  Future<List<General>> execute() async {
+    return await _repository.get();
   }
 }
