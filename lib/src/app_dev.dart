1a77c94a041f1e976cb433aadc920e06cb6fb8f3
Russell Eduardo Soza Arevalo <endosoza1@gmail.com>
2023-12-04 21:39:50 -0600

    feat: Add blocProviders and initialize locator (#60)

--- lib/src/app_dev.dart
+++ lib/src/app_dev.dart
@@ -1,17 +1,31 @@
 import 'package:birthflow/src/config/router/app_router.dart';
-import 'package:flutter/material.dart';
 
-class AppDev extends StatelessWidget {            
-          
-  final _appRouter = AppRouter();            
-            
-  @override            
-  Widget build(BuildContext context){            
-    return MaterialApp.router(       
-      title: 'Birth Flow',
-      theme: ThemeData(useMaterial3: true),
-      debugShowCheckedModeBanner: false,     
-      routerConfig: _appRouter.config(),         
-    );            
-  }            
-}            
+import 'package:birthflow/src/core/general/domain/usecases/get_usecase.dart';
+import 'package:birthflow/src/locator.dart';
+import 'package:birthflow/src/presentation/home/bloc/bloc.dart';
+import 'package:birthflow/src/presentation/home/bloc/state/state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppDev extends StatelessWidget {
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<GeneralBloc>(
+          create: (context) => GeneralBloc(
+            locator<GeneralGetUseCase>(),
+          )..add(const GeneralEvent.fetchGeneralData()),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Birth Flow v1',
+        theme: ThemeData(useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        routerConfig: _appRouter.config(),
+      ),
+    );
+  }
+}
