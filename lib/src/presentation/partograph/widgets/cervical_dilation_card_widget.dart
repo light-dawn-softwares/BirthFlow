c5c36140308b0acfcc460f6faf6d732a59c72894
Russell Soza <endosoza1@gmail.com>
2023-12-23 21:41:02 -0600

    refactor: Update interface

--- lib/src/presentation/partograph/widgets/cervical_dilation_card_widget.dart
+++ lib/src/presentation/partograph/widgets/cervical_dilation_card_widget.dart
@@ -1,7 +1,9 @@
+import 'package:birthflow/src/core/cervical_dilation/domain/models/cervical_dilation.dart';
 import 'package:flutter/material.dart';
 
 class CervicalDilationCardWidget extends StatefulWidget {
-  const CervicalDilationCardWidget({super.key});
+  const CervicalDilationCardWidget({super.key, required this.list});
+  final List<CervicalDilation> list;
 
   @override
   State<CervicalDilationCardWidget> createState() =>
@@ -11,89 +13,91 @@ class CervicalDilationCardWidget extends StatefulWidget {
 class _CervicalDilationCardState extends State<CervicalDilationCardWidget> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Dilataciones cervicales',
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Añadir'),
-            ),
-          ],
-        ),
-        DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Dilatacion',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
+    if (widget.list.isEmpty == false) {
+      return Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Dilataciones cervicales',
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
-            ),
-            DataColumn(
-              label: Expanded(
-                flex: 2,
-                child: Text(
-                  'Ram o Rem',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Añadir'),
               ),
-            ),
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Fecha y Hora',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-          ],
-          rows: <DataRow>[
-            DataRow(
-              cells: <DataCell>[
-                const DataCell(Text('5')),
-                DataCell(
-                  Checkbox(
-                    value: true,
-                    onChanged: (bool? value) {},
+            ],
+          ),
+          DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Expanded(
+                  child: Text(
+                    'Dilatacion',
+                    style: TextStyle(fontStyle: FontStyle.italic),
                   ),
                 ),
-                DataCell(Text(DateTime.now().toString())),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                const DataCell(Text('7')),
-                DataCell(
-                  Checkbox(
-                    value: true,
-                    onChanged: (bool? value) {},
+              ),
+              DataColumn(
+                label: Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Ram o Rem',
+                    style: TextStyle(fontStyle: FontStyle.italic),
                   ),
                 ),
-                DataCell(Text(DateTime.now().toString())),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                const DataCell(Text('6')),
-                DataCell(
-                  Checkbox(
-                    value: true,
-                    onChanged: (bool? value) {},
+              ),
+              DataColumn(
+                label: Expanded(
+                  child: Text(
+                    'Fecha y Hora',
+                    style: TextStyle(fontStyle: FontStyle.italic),
                   ),
                 ),
-                DataCell(Text(DateTime.now().toString())),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
+              ),
+            ],
+            rows: widget.list
+                .map(
+                  (cervicalDilation) => DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text(cervicalDilation.value.toString())),
+                      DataCell(
+                        Checkbox(
+                          value: cervicalDilation.ramOrRem,
+                          onChanged: (bool? value) {},
+                        ),
+                      ),
+                      DataCell(Text(cervicalDilation.time.toString())),
+                    ],
+                  ),
+                )
+                .toList(),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Dilataciones cervicales',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Añadir'),
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Agregue una dilatacion cervical'),
+          ),
+        ],
+      );
+    }
   }
 }
